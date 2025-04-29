61b4e07b1728576a39d67f0ef1599ed41ca2ae5c
Rodrigo Arce C <rodrigoar87@gmail.com>
2025-04-28 19:53:00 -0600

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,1 +1,17 @@
+function combo1(){
 
+var opcion=prompt("Venta de cafe\n1-Combo 1 "+
+        "\n2-Combo 2\n3-combo 3"+"\ns- salir  ");
+}
+
+
+
+function changeImage(image) {
+image.src = 'img/houseing.png';
+var audio = new Audio('media/door.mp3');
+audio.play();
+}
+function timbre(){
+var audio = new Audio('media/timbre.mp3');
+audio.play();
+}
